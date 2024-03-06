03277b776d19c36d1469eacfb0783ea72bdd6f18
bartekk <bartosz.ksiazek@microsoft.wsei.edu.pl>
2024-03-06 11:28:51 +0100

    Initial commit

---
+++ Web/Pages/Quiz/Item.cshtml.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
+
+namespace BackendLab01.Pages
+{
+    
+    public class QuizModel : PageModel
+    {
+        private readonly IQuizUserService _userService;
+
+        private readonly ILogger _logger;
+        public QuizModel(IQuizUserService userService, ILogger<QuizModel> logger)
+        {
+            _userService = userService;
+            _logger = logger;
+        }
+        [BindProperty]
+        public string Question { get; set; }
+        [BindProperty]
+        public List<string> Answers { get; set; }
+        
+        [BindProperty]
+        public String UserAnswer { get; set; }
+        
+        [BindProperty]
+        public int QuizId { get; set; }
+        
+        [BindProperty]
+        public int ItemId { get; set; }
+        
+        public void OnGet(int quizId, int itemId)
+        {
+            QuizId = quizId;
+            ItemId = itemId;
+            var quiz = _userService.FindQuizById(quizId);
+            var quizItem = quiz?.Items[itemId - 1];
+            Question = quizItem?.Question;
+            Answers = new List<string>();
+            if (quizItem is not null)
+            {
+                Answers.AddRange(quizItem?.IncorrectAnswers);
+                Answers.Add(quizItem?.CorrectAnswer);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("Item", new {quizId = QuizId, itemId = ItemId + 1});
+        }
+    }
+}
---
+++ Web/SeedData.cs
@@ -0,0 +1,37 @@
+﻿using ApplicationCore.Interfaces.Repository;
+using BackendLab01;
+
+namespace Infrastructure.Memory;
+
+public static class SeedData
+{
+    public static void Seed(this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var provider = scope.ServiceProvider;
+            var quizRepo = provider.GetService<IGenericRepository<Quiz, int>>();
+            var quizItemRepo = provider.GetService<IGenericRepository<QuizItem, int>>();
+
+            var quiz1Items = new List<QuizItem>
+            {
+                new QuizItem(1, "What color is grass", new List<string>{"blue","pink","purple"}, "green"),
+                new QuizItem(2, "What color is monkey", new List<string>{"pink","brown","red"}, "black"),
+                new QuizItem(3, "What color is lemon", new List<string>{"red","green","grey"}, "yellow")
+            };
+            var quiz2Items = new List<QuizItem>
+            {
+                new QuizItem(1, "Baptism of poland year", new List<string>{"123","321","333"}, "966"),
+                new QuizItem(2, "WW2 year", new List<string>{"1932","2323","1111"}, "1939"),
+                new QuizItem(3, "grunwald war year", new List<string>{"1234","1111","1000"}, "1410")
+            };
+            
+            var quiz1 = new Quiz(1, quiz1Items, "Quiz 1");
+            var quiz2 = new Quiz(2, quiz2Items, "Quiz 2");
+            quizRepo.Add(quiz1);
+            quizRepo.Add(quiz2);
+        } 
+    }
+}
+
+
