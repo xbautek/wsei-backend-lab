5f24861093f240ed130ab524ad24dce0aa968c50
bartekk <bartosz.ksiazek@microsoft.wsei.edu.pl>
2024-03-20 10:30:41 +0100

    backendlab swagger

---
+++ WebApi/Controllers/ApiUserQuizController.cs
@@ -0,0 +1,24 @@
+﻿
+using BackendLab01;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("/api/v1/quizzes")]
+public class ApiUserQuizController : Controller
+{
+    private IQuizUserService _userService;
+
+    public ApiUserQuizController(IQuizUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public Quiz GetQuizById(int id)
+    {
+        return _userService.FindQuizById(id);
+    }
+}
